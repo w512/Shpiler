7b8f994ba5d621e9a7ff0e341393d5c251c91a04
Nikolay Blohin <nikolay@blohin.org>
2011-06-22 22:26:35 +0300

    Implemented move the cursor.

--- src/shpiler.py
+++ src/shpiler.py
@@ -13,15 +13,18 @@ class Cursor(cocos.layer.ColorLayer):
         super(Cursor, self).__init__(128, 224, 128, 255)
         self.run_status = False # if false - stop, if true - run
         self.direction = 1      # 1 - North, 2 - East, 3 - South, 4 - West
-        self.m_s = 5            # step for move
+        self.m_s = 3            # step for move
         self.m_d = 0.01         # duration for move
 
+        self.win_width, self.win_height = cocos.director.director.get_window_size()
+
         self.sprite = cocos.sprite.Sprite('cursor.png')
-        self.sprite.position = 320,40
+        x = int(self.win_width/2)
+        y = 8
+        self.sprite.position = x, y
         self.add(self.sprite)
 
-        print cocos.director.director.get_window_size()
-
+        self.schedule(self.update)
         
 
     def on_key_press(self, key, modifiers):
@@ -32,17 +35,30 @@ class Cursor(cocos.layer.ColorLayer):
         repeat_down = Repeat(MoveBy((0, -1*self.m_s), duration=self.m_d))
         if key==65361:
             self.sprite.do(repeat_left)
+            self.direction = 4
         elif key==65362:
             self.sprite.do(repeat_up)
+            self.direction = 1
         elif key==65363:
             self.sprite.do(repeat_right)
+            self.direction = 2
         elif key==65364:
             self.sprite.do(repeat_down)
+            self.direction = 3
         elif key==32:
             self.sprite.stop()
 
-    def update(self):
-        pass
+    def update(self, *args, **kwargs):
+        x = self.sprite.x
+        y = self.sprite.y
+        if self.sprite.contains(1, y) and self.direction==4:
+            self.sprite.stop()
+        elif self.sprite.contains(self.win_width-1, y) and self.direction==2:
+            self.sprite.stop()
+        elif self.sprite.contains(x, 1) and self.direction==3:
+            self.sprite.stop()
+        elif self.sprite.contains(x, self.win_height-1) and self.direction==1:
+            self.sprite.stop()
         
 
 if __name__ == "__main__":
