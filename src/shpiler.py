dd9e5046a9fe9c8e1dab3241abfa4703a483ae11
Nikolay Blohin <nikolay@blohin.org>
2011-06-22 23:43:44 +0300

    Added scoring.

--- src/shpiler.py
+++ src/shpiler.py
@@ -27,22 +27,32 @@ class Cursor(cocos.layer.ColorLayer):
         self.direction = 1      # 1 - North, 2 - East, 3 - South, 4 - West
         self.m_s = 3            # step for move
         self.m_d = 0.01         # duration for move
+        self.score = 0          # player's score
 
         self.win_width, self.win_height = cocos.director.director.get_window_size()
+
+        self.label = cocos.text.Label('Score: 0',
+                          font_name='Times New Roman',
+                          font_size=16,
+                          color=(0, 0, 0, 200),
+                          x=3,
+                          y=self.win_height-20)
+
 
         self.sprite = cocos.sprite.Sprite('cursor.png')
         self.sprite_2 = cocos.sprite.Sprite('target.png')
         x = int(self.win_width/2)
         y = 8
         self.sprite.position = x, y
-        self.add(self.sprite)
 
         x = random.randrange(10, self.win_width-10)
         y = random.randrange(30, self.win_height-10)
         self.sprite_2.position = x, y
-        self.add(self.sprite_2)
-        
 
+        self.add(self.sprite)        
+        self.add(self.sprite_2)
+        self.add(self.label)
+        
         self.schedule(self.update)
         self.schedule_interval(self.move_target, 3)
         
@@ -72,11 +82,11 @@ class Cursor(cocos.layer.ColorLayer):
         x = random.randrange(10, self.win_width-10)
         y = random.randrange(10, self.win_height-10)
         self.sprite_2.do(MoveTo((x, y), duration=3.5))
-        print x, y
 
     def update(self, *args, **kwargs):
         x = self.sprite.x
         y = self.sprite.y
+        # check borders
         if self.sprite.contains(1, y) and self.direction==4:
             self.sprite.stop()
         elif self.sprite.contains(self.win_width-1, y) and self.direction==2:
@@ -85,9 +95,17 @@ class Cursor(cocos.layer.ColorLayer):
             self.sprite.stop()
         elif self.sprite.contains(x, self.win_height-1) and self.direction==1:
             self.sprite.stop()
+
+        # check target
+        x = self.sprite_2.x
+        y = self.sprite_2.y        
+        if self.sprite.contains(x, y):
+            self.score += 1
+            self.label.element.text = 'Score: %s' % self.score
+            self.move_target()
         
 
-if __name__ == "__main__":
+if __name__=="__main__":
     cocos.director.director.init(caption='Shpiler')
     main_scene = cocos.scene.Scene(Cursor())
     cocos.director.director.run(main_scene)
